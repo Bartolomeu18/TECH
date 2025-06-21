7a4cf938e71c04cb4feb5ca129af7e224a2ed366
Bartolomeu Sebastião <153238440+Bartolomeu18@users.noreply.github.com>
2025-06-21 18:32:00 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+
+function displayMenu() {
+    
+var menu = document.getElementById('menuitens')
+if (menu.style.display=='flex') {
+    menu.style.display='none'
+}else{
+    menu.style.display='flex'
+
+}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TECH
+portfolio for Benoltech
