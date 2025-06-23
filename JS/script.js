8af9f9dcb5b41847e2709c0b523329c7d330504e
Bartolomeu Sebastião <153238440+Bartolomeu18@users.noreply.github.com>
2025-06-23 19:36:30 +0100

    loading efect

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,33 @@
 function displayMenu() {
     
 var menu = document.getElementById('menuitens')
-if (menu.style.width=='45%') {
+if (menu.style.width=='50%') {
     menu.style.width='0%'
 }else{
-    menu.style.width='45%'
+    menu.style.width='50%'
 
 }
 }
+
+//efito loading
+
+// Simula o carregamento da página (remova isso em produção)
+function simulateLoading() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, 2000); // 2 segundos de simulação
+    });
+}
+
+// Quando a página estiver totalmente carregada
+window.addEventListener('load', async function() {
+    // Simula um tempo de carregamento (remova em produção)
+    await simulateLoading();
+    
+    // Esconde o loading
+    document.getElementById('loading').style.display = 'none';
+    
+    // Mostra o conteúdo
+    document.getElementById('content').style.display = 'block';
+});
